ee39395dae1f29d44ec866688231250e5ae156dd
Michelk-byte <michel.khoury.w@hotmail.com>
2021-05-14 11:03:51 +0300

    update page numbers

--- src/main/java/com/example/demo/ElectronicLocatorApplication.java
+++ src/main/java/com/example/demo/ElectronicLocatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantLocatorApplication {
+public class ElectronicLocatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestaurantLocatorApplication.class, args);
+        SpringApplication.run(ElectronicLocatorApplication.class, args);
     }
 
 }
